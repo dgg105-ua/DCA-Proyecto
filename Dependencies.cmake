b232092f87a3739a312d80ab42784cf3798f3571
mlc107ua <mlc107@gcloud.ua.es>
2026-01-11 22:36:02 +0100

    Agregado el sonido a la app

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -11,13 +11,14 @@ if (NOT raylib_FOUND)
 		raylib
 		GIT_REPOSITORY https://github.com/raysan5/raylib
 		GIT_TAG 5.5
+		GIT_SHALLOW ON
 	)
 	
 	set(BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
 	set(BUILD_GAMES OFF CACHE BOOL "" FORCE)
 
 	# Inform the user that raylib will be fetched/built (can take some time)
-	message(STATUS "Instalando/descargando y construyendo raylib (esto puede tardar varios minutos)...")
+	message(STATUS "Descargando raylib con shallow clone (mucho más rápido)...")
 
 	FetchContent_MakeAvailable(raylib)	
 	
